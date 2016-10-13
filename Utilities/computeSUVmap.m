c6130e375154ec76e728e6b66b319d12d6c067eb
Martin Vallières <mart.vallieres@gmail.com>
2016-10-13 17:08:06 -0400

    Correction to computeSUVmap.m

--- Utilities/computeSUVmap.m
+++ Utilities/computeSUVmap.m
@@ -80,14 +80,12 @@ SUVmap = rawPET*weight/injected_dose_decay;
 end
 
 % CERR UTILITY FUNCTION (can be found at: https://github.com/adityaapte/CERR)
-function [totSec hh mm ss fract] = dcm_hhmmss(dateStr)
+function [totSec] = dcm_hhmmss(dateStr)
 if ~ischar(dateStr)
     dateStr = num2str(dateStr);
 end
 hh = str2double(dateStr(1,1:2));
 mm = str2double(dateStr(1,3:4));
 ss = str2double(dateStr(1,5:6));
-fract_start = find(dateStr,'.');
-fract = str2double(dateStr(1,fract_start+1:end));
 totSec = hh*60*60 + mm*60 + ss;
 end
